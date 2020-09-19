810c91ecd730c78733ab7aceef9f581844b1dfb7
pedro.silva <phmastelaro@gmail.com>
2020-09-19 18:34:04 -0300

    Adjust warnings

--- blog/src/Social/Social.js
+++ blog/src/Social/Social.js
@@ -13,25 +13,53 @@ import {
 function Social () {
   return (
     <div className='Social'>
-      <a href='https://github.com/pedrokuhn' target='_blank'>
+      <a
+        href='https://github.com/pedrokuhn'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <GitHub className='icon' />
       </a>
-      <a href='https://twitter.com/PedroKuhn_' target='_blank'>
+      <a
+        href='https://twitter.com/PedroKuhn_'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <Twitter className='icon' />
       </a>
-      <a href='https://www.facebook.com/OficialPedroKuhn' target='_blank'>
+      <a
+        href='https://www.facebook.com/OficialPedroKuhn'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <Facebook className='icon' />
       </a>
-      <a href='https://telegram.me/PedroKuhn' target='_blank'>
+      <a
+        href='https://telegram.me/PedroKuhn'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <Telegram className='icon' />
       </a>
-      <a href='mailto:phmastelaro@gmail.com' target='_blank'>
+      <a
+        href='mailto:phmastelaro@gmail.com'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <Email className='icon' />
       </a>
-      <a href='https://www.instagram.com/kuhn_pedro/' target='_blank'>
+      <a
+        href='https://www.instagram.com/kuhn_pedro/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <Instagram className='icon' />
       </a>
-      <a href='https://www.linkedin.com/in/kuhnpedro/' target='_blank'>
+      <a
+        href='https://www.linkedin.com/in/kuhnpedro/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         <LinkedIn className='icon' />
       </a>
     </div>
